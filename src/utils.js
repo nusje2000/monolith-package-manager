5412de637322a90d577786e3964ac40b85846d78
Alex van den Oudenhoven <lexouden@outlook.com>
2020-12-31 15:47:59 +0100

    🔵path variable fix; added ora package

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 
 module.exports.getConfigFile = (path) => {
-  const config = require(configPath);
+  const config = require(path);
 
   if (!config || !(config instanceof Object)) throw chalk `{red An invalid config file was given or no config file was found at} {blueBright ${path}}`;
 
